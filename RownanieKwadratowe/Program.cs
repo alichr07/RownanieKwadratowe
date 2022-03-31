1daa37f04888891a442ca5188a9d419e7d2f2fa6
Alicja <alichr07@gmail.com>
2022-03-31 21:10:58 +0200

    Dodaj pliki projektów.

---
+++ RownanieKwadratowe/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RownanieKwadratowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Obliczanie ilości pierwiastków równania kwadratowego
+
+            Console.WriteLine("Podaj a: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c: ");
+            double c = double.Parse(Console.ReadLine());
+
+            double delta = b * b - 4 * a * c;
+
+            if (delta > 0)
+            {
+                Console.WriteLine("2 pierwiastki");
+            }
+            else if (delta == 0)
+            {
+                Console.WriteLine("1 pierwiastek");
+            }
+            else
+            {
+                Console.WriteLine("0 pierwiastków");
+            }
+            Console.ReadKey();
+        }
+    }
+}
